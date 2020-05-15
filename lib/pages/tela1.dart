702c3b4bad28eccdd5ebcf66837b4e89d0af9753
Adriano Martins <ampontes93@gmail.com>
2020-05-15 15:11:46 -0300

    removendo o banner de debug e MaterialApp desnecessário

--- lib/pages/tela1.dart
+++ lib/pages/tela1.dart
@@ -5,32 +5,30 @@ class Tela1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home:Scaffold(
-           body: ListView(
-            padding:  EdgeInsets.all(32.0),
-            children: <Widget>[
-              const SizedBox(height: 20), //espaço entre linhas
-              Image.asset('image/pena.png',),
-              const SizedBox(height: 50), //espaço entre linhas
-              Text ('Pratique as questões de TI do Enade do Curso de Secretariado Executivo  \n \n \n', 
-              textAlign: TextAlign.center, style: TextStyle(fontSize: 17.0),),
-                
+    return Scaffold(
+      body: ListView(
+        padding:  EdgeInsets.all(32.0),
+        children: <Widget>[
+          const SizedBox(height: 20), //espaço entre linhas
+          Image.asset('image/pena.png',),
+          const SizedBox(height: 50), //espaço entre linhas
+          Text ('Pratique as questões de TI do Enade do Curso de Secretariado Executivo  \n \n \n',
+            textAlign: TextAlign.center, style: TextStyle(fontSize: 17.0),),
+
           RaisedButton(
-              child: Text('INICIAR', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold ),),                           
-              elevation:5.0, //sombra botão
-              shape: new RoundedRectangleBorder( borderRadius: new BorderRadius.circular(60.0), 
+            child: Text('INICIAR', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold ),),
+            elevation:5.0, //sombra botão
+            shape: new RoundedRectangleBorder( borderRadius: new BorderRadius.circular(60.0),
               side: BorderSide(color: Colors.grey[500]),),
-              textColor: Colors.orange[800],
-              onPressed: () => Navigator.push(context,
-                  MaterialPageRoute(
-                    builder: (context) => Telabutton(),
+            textColor: Colors.orange[800],
+            onPressed: () => Navigator.push(context,
+              MaterialPageRoute(
+                builder: (context) => Telabutton(),
               ),
-              ),
+            ),
           ),
         ],
       ),
-    ),
-  );
+    );
 }
 }
